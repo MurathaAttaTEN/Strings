b0dc67b473b45b55377ceee565d3b4de332a3696
MurathaAttaTEN <attamuratha@gmail.com>
2022-03-10 08:56:30 +0300

    today

--- src/main.kt
+++ src/main.kt
@@ -1,38 +1,39 @@
+import java.time.Year
 import kotlin.math.max
 import kotlin.reflect.jvm.internal.impl.resolve.sam.SamConversionResolverImpl
 
 fun main(){
-var stmt = greetings("moon",4)
-    println(stmt)
-    name()
-    var chars = place("blue")
-    println(chars)
-    sent()
-}
-fun greetings(name:String, age: Int):String{
+stmt()
+    zero()
+    years("wanjiru", 100)
+    println()
+    place()
 
-    var stmt = "hi,my name is $name and i am $age years old"
-    return stmt
+
+
+
 
 
 }
-
-fun name(){
-    var chars= "akirachix"
-    var stmt= chars[1].toString()+chars[3]+chars[4]
+fun stmt(){
+    var chars = "codeHive"
+    var stmt = chars[5].toString()+ chars[6]+chars[7]
     println(stmt)
 
+}
+fun zero(){
+    var x = 15
+    var y = 6
+    var M = 15%6
+    println(M)
+}
+fun years(name: String,age: Int):String{
+    var ring = "Hi,my name is p and i am z years old."
+    println(ring)
+    return ring
 
 }
-fun place(colors: String): Int{
-    var chars= colors.length
-    return chars
-}
-fun sent(){
-    var name = "Atta"
-    if(name=="Atta"){
-        println("That's me")}
-    else
-        println("I don't know who that is")
-
+fun place(){
+    var p = "country"
+    println("country".length)
 }
